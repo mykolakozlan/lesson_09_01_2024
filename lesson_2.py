cde80dd70c48eeff3ae08c65c01fc3003c3985f9
nikolaskozlan <maxsuccess4@gmail.com>
2024-01-12 21:43:28 +0200

    added lesson 2

---
+++ lesson_2.py
@@ -0,0 +1,116 @@
+# Data types
+
+# Anmutable/Не змінні
+
+#Numeric
+
+# int - Integer    1
+# float -     1.3        0.00001
+# decimal  0.01         0.00001
+# complex -    (1 + 3j)
+
+# bool - logic operator(True/False)
+# str String
+# range
+# None
+
+# tuple()
+
+
+# Mutable/Змінні
+
+# list()
+# dict()
+# set()
+
+#
+# box_number_1 = True   #snake case
+#
+# a = 1
+# b = 2
+# c = a + b
+#
+# print("World")
+#
+# my_print = "3333"
+
+
+
+# number_1 = 1
+#
+# # number_2 = number_1
+# print(number_1, type(number_1), id(number_1))
+#
+# number_1 = 3
+#
+# print(number_1, type(number_1), id(number_1))
+
+
+
+
+# list_1 = list()
+#
+# list_2 = list_1
+#
+# list_1 = []
+#
+# print(list_2)
+
+
+# number_1 = 10.3
+# number_2 = 3.3
+#
+# result_add = number_1 + number_2
+
+# number_1 += number_2
+# number_1 = number_1 + number_2
+
+
+# result_sub = number_1 - number_2
+# result_mul = number_1 * number_2
+# result_div = number_1 / number_2
+#
+# result_div_2 = number_1 // number_2   # цілочислене ділення
+# result_div_3 = number_1 % number_2      # залишок від ділення
+#
+# result_div_4 = divmod(number_1, number_2) # містить в собі цілочислене ділення та залишок від ділення
+#
+#
+# result_power = number_1 ** number_2   # ступінь
+# result_root = number_1 ** 0.5   # ступінь
+#
+# # print(result_div_4, type(result_root))
+# print(result_div_4)
+
+# value_str = 'hello'
+# value_str = "hello"
+# value_str = '''hello'''
+# value_str = """hello"""
+
+
+
+
+# value_str = "hello I'm"
+# name = "1.444"
+#
+# greetings_1 = value_str + " " + name
+# # greetings_2 = f"hello I'm {name}"
+#
+# print(greetings_1)
+
+
+
+###### Constants ########
+
+NUMBER_OF_PI = 3.14
+MONTHS_IN_YEAR = 12
+
+
+
+############# input() ##########   ALWAYS STRING
+
+# some_num_1 = input("Type some number: ")
+# some_num_2 = input("Type some number: ")
+#
+# result = int(some_num_1) + int(some_num_2)
+# print(result)
